8b347cb59e78049c504786643e17f12baaf02b1f
Mariam Mohammad Ali <maryamhoth@gmail.com>
2024-03-12 12:19:16 +0000

    add cors

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 require('dotenv').config()
 require('./config/database')
+var cors = require('cors')
 
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
@@ -16,7 +17,7 @@ var app = express()
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
